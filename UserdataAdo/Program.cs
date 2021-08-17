f088ad87f29a4b2538a7e7e90d14485318460175
smomin156 <63006524+smomin156@users.noreply.github.com>
2021-08-18 00:44:01 +0530

    Add files via upload

---
+++ UserdataAdo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbLibrary;
+using System.Configuration;
+
+namespace UserdataAdo
+{
+    class Program
+    {
+        DataStore dataStore = new DbLibrary.DataStore(ConfigurationManager.ConnectionStrings["TrainingConnection"].ConnectionString);
+        DisconnectedStore disconnectedStore = new DbLibrary.DisconnectedStore(ConfigurationManager.ConnectionStrings["TrainingConnection"].ConnectionString);
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Console.WriteLine("Enter username");
+            string username = Console.ReadLine();
+            Console.WriteLine("Enter password");
+            string password = Console.ReadLine();
+            //program.TestUser(username,password);
+            program.TestDisconnected(username, password);
+            Console.ReadLine();
+        }
+
+        public void TestUser(string username,string password)
+        {
+            try
+            {
+                string res = (dataStore.GetUserRole(username, password)).ToString();
+                Console.WriteLine("User Role is :" + res);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void TestDisconnected(string username,string password)
+        {
+           string result=(disconnectedStore.CheckUser(username, password)).ToString();
+           Console.WriteLine("User Role is :" + result);
+        }
+    }
+}
